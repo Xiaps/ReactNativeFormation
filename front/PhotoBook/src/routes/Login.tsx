8e8882b75a7c7508edf627d11c4fa2bcf8806f80
Maxime Belouin <maxime.belouin@gmail.com>
2022-03-09 14:36:26 +0100

    Ajout du formulaire d'identification

--- front/PhotoBook/src/routes/Login.tsx
+++ front/PhotoBook/src/routes/Login.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
-import {Button} from 'react-native-elements';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, Input} from 'react-native-elements';
 import {api} from '../api';
 import {ScreenProps} from '../navigation';
 import {useAppDispatch, useAppSelector} from '../redux/hooks';
@@ -63,7 +63,52 @@ const Login = ({navigation}: ScreenProps<'Login'>) => {
 
   return (
     <View>
-      <Text>Login works</Text>
+      <Controller
+        control={control}
+        rules={{
+          required: 'Email required',
+          pattern: {
+            value:
+              /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            message: 'Please enter a valid email',
+          },
+        }}
+        render={({field: {onChange, onBlur, value}}) => (
+          <Input
+            placeholder="Email"
+            errorStyle={{color: 'red'}}
+            errorMessage={errors.email && errors.email.message}
+            autoCompleteType={undefined}
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="email"
+      />
+
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({field: {onChange, onBlur, value}}) => (
+          <Input
+            placeholder="Password"
+            errorStyle={{color: 'red'}}
+            errorMessage={errors.password && 'Error format'}
+            autoCompleteType={undefined}
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            secureTextEntry={true}
+          />
+        )}
+        name="password"
+      />
+
       <Button
         title="Connect"
         onPress={handleSubmit(onSubmit)}
@@ -73,5 +118,9 @@ const Login = ({navigation}: ScreenProps<'Login'>) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {},
+});
 
 export default Login;
